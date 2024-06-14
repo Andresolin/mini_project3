60539a31c271b97b578415e0c78cbeaaedffdc54
Andre Permana Solin <permanasolin@gmail.com>
2024-06-14 14:39:11 +0700

    Memproses data string sebagai JSON

---
+++ app/src/main/java/org/d3if3002/mini_project3/model/City.kt
@@ -0,0 +1,7 @@
+package org.d3if3002.mini_project3.model
+
+data class City(
+    val city: String,
+    val country: String,
+    val imageId: String
+)
--- app/src/main/java/org/d3if3002/mini_project3/network/CityApiService.kt
+++ app/src/main/java/org/d3if3002/mini_project3/network/CityApiService.kt
@@ -1,20 +1,28 @@
 package org.d3if3002.mini_project3.network
 
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import org.d3if3002.mini_project3.model.City
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL = "https://raw.githubusercontent.com/Andresolin/Static-JSON/main/"
 
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 
 interface CityApiService {
     @GET("static-api.json")
-    suspend fun getCity(): String
+    suspend fun getCity(): List<City>
 }
 
 object CityApi {
