e78646b3981c3b07d16d575a0bead7d779ee55a9
Andre Permana Solin <permanasolin@gmail.com>
2024-06-14 14:27:18 +0700

    Membuat logo aplikasi

--- app/src/main/java/org/d3if3002/mini_project3/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3002/mini_project3/ui/screen/MainScreen.kt
@@ -1,9 +1,12 @@
 package org.d3if3002.mini_project3.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -11,7 +14,9 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -23,8 +28,16 @@ import org.d3if3002.mini_project3.R
 fun MainScreen() {
     Scaffold(
         topBar = {
-            TopAppBar(title = {
-                Text(text = stringResource(id = R.string.app_name))
+            TopAppBar(
+                title = {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Image(
+                            painter = painterResource(id = R.drawable.city),
+                            contentDescription = stringResource(id = R.string.logo),
+                            modifier = Modifier.size(60.dp)
+                        )
+                        Text(text = stringResource(id = R.string.app_name), modifier = Modifier.padding(start = 8.dp))
+                    }
             },
                 colors = TopAppBarDefaults.mediumTopAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primaryContainer,
